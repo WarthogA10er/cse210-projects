3a86de23190c2ac4e7ab43a12de8768e47ac2d8a
James Turner <jturner2@byupathway.edu>
2025-03-08 10:50:33 -0700

    completed W01E2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //store grade percent
+        Console.WriteLine("What is your Grade Percentage?:");
+        string percent = Console.ReadLine();
+        
+        //convert str to int
+        int percentNumber = int.Parse(percent);
+
+        //set letter to neutral
+        string letter = "";
+        //logic to find grade
+        if (percentNumber >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentNumber >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentNumber >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentNumber >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (percentNumber < 60)
+        {
+           letter = "F";
+        }
+
+        //set sign to neutral
+        string sign = "";
+        
+        //logic to exclude extreme grades
+        if (percentNumber < 96 && percentNumber >59)
+        {
+            //logic for + or -
+            int remainder = percentNumber % 10;
+            if (remainder >= 7)
+            {
+                sign = "+";
+            }
+
+            else if (remainder < 3)
+            {
+                sign = "-";
+            }
+        }
+        
+        //logic for pass or fail
+        if (percentNumber >= 70)
+        {
+            Console.WriteLine("Good job, you passed!");
+        }
+
+        else if (percentNumber < 70)
+        {
+            Console.WriteLine("You'll get it next time.");
+        }
+
+        //display grade
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+        
     }
 }
