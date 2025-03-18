05d66dedcffa081f429231d0a48507430565b3e0
James Turner <jturner2@byupathway.edu>
2025-03-17 21:15:30 -0700

    Completed assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Dennington Decor";
+        job1._jobTitle = "Warehouse Hand";
+        job1._startYear = "2020";
+        job1._endYear = "2021";
+
+        Job job2 = new Job();
+        job2._company = "Blue Pearl";
+        job2._jobTitle = "Veterinary Assistant";
+        job2._startYear = "2022";
+        job2._endYear = "2025";
+    
+        Resume resume1 = new Resume();
+        resume1._name = "James Turner";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+       resume1.Display();
     }
 }
