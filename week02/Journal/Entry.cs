d03a9193486fdfa253ab0b3d9aaa5f449e5cc012
James Turner <jturner2@byupathway.edu>
2025-03-26 18:31:53 -0700

    my code up until this point

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,33 +1,12 @@
-// public class Entry
-//     {
-//         public string _date;
-//         public string _promptText;
-//         public string _entryText;
-
-//         public void Display()
-//         {
-//            Console.WriteLine($"Date: {_date}. Prompt: {_promptText}. Journal Entry: {_entryText}");
-//         }
-//     }
-
-// AI Example
-using System;
-
 public class Entry
-{
-    public string Prompt { get; set; }
-    public string Response { get; set; }
-    public string Date { get; set; }
-
-    public Entry(string prompt, string response, string date)
     {
-        Prompt = prompt;
-        Response = response;
-        Date = date;
+        public string _date;
+        public string _promptText;
+        public string _entryText;
+
+        public void Display()
+        {
+           Console.WriteLine($"Date: {_date}. Prompt: {_promptText}. Journal Entry: {_entryText}");
+        }
     }
 
-    public override string ToString()
-    {
-        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
-    }
-}
